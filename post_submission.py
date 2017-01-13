6efd389d5fc514c3c57f050400ab931e7acc59bd
Fleuri <laur1.suomalainen@gmail.com>
2017-01-13 15:33:15 +0200

    Fixed an argument bug

--- post_submission.py
+++ post_submission.py
@@ -1,12 +1,15 @@
 import praw
 import sys
 
-if len(sys.argv) > 1 and len(sys.argv) < 4:
-    reddit = praw.Reddit('bot1')
-    subreddit = reddit.subreddit('pythonforengineers')
-    submission = subreddit.submit(sys.argv[1],
-                     sys.argv[2])
-    submission.mod.sticky()
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit('apocalypseworld')
+submission = None
+if len(sys.argv) == 2:
+    submission = subreddit.submit(sys.argv[1])
+elif len(sys.argv == 3):
+    submission = subreddit.submit(sys.argv[1], sys.argv[2])
+
 else:
     print("You must provide arguments (Title, <selftext>)")
 
+submission.mod.sticky()
