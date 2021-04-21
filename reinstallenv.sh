633f115cbf701f144e7ef8c685d33e44d967dbe5
Fleuri <laur1.suomalainen@gmail.com>
2021-04-21 18:25:49 +0300

    Modified reinstallenv

--- reinstallenv.sh
+++ reinstallenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rm -rf ./env
-virtualenv -p /usr/bin/python3.5 ./env
+virtualenv -p /usr/local/bin/python3.5 ./env
 ./env/bin/pip install praw
-cp ~/praw.ini ./env/lib/python3.5/site-packages/praw/praw.ini
+cat ~/praw.ini >> ./env/lib/python3.5/site-packages/praw/praw.ini
 
